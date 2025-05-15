5a1f1e3f5df553faceedc8999cbb5b647520a365
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-05-15 06:20:06 -0700

    Try adding a CPU runner to CI
    Signed-off-by: Rory Mitchell <r.a.mitchell.nz@gmail.com>

---
+++ ci/run_ctests_cpu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+# Support customizing the ctests' install location.  Use the build dir if
+# it seems to exist, otherwise assume we installed the tests.
+if [[ -f "${LIBLEGATE_DATAFRAME_BUILD_DIR:-./cpp/build}/gtests/cpp_tests" ]]; then
+    cd "${LIBLEGATE_DATAFRAME_BUILD_DIR:-./cpp/build}/gtests"
+else
+    cd "${INSTALL_PREFIX:-${CONDA_PREFIX:-/usr}}/bin/gtests/liblegate_dataframe/"
+fi
+
+# Unless `LEGATE_CONFIG` is set, default to all available GPUs and set fbmem/sysmem.
+# LEGATE_TEST=1 to test broadcasting code paths (locally).
+LEGATE_CONFIG=${LEGATE_CONFIG:- --cpus 8 --sysmem=6000 --omps=0} \
+LEGATE_TEST=${LEGATE_TEST:-1} \
+legate ./cpp_tests --output-on-failure --no-tests=error "$@" --gtest_filter="*CSV*"
