59bb5e7ff3b0d0d1427da7c8153231a28be4d1af
Sebastian Berg <sebastianb@nvidia.com>
2025-07-14 03:26:11 -0700

    cleanup and filter files rather than using -k for cpu tests
    Signed-off-by: Sebastian Berg <sebastianb@nvidia.com>

--- python/legate_dataframe/ldf_polars/__init__.py
+++ python/legate_dataframe/ldf_polars/__init__.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import traceback
-from contextlib import nullcontext
 from functools import partial
 from typing import Any
 
@@ -70,29 +69,7 @@ class LegateOperations:
         return collect_with_legate(self._ldf)
 
 
-def _callback(
-    ir,
-    with_columns: list[str] | None,
-    pyarrow_predicate: str | None,
-    n_rows: int | None,
-    *,
-    device=None,
-    memory_resource=None,
-):
-    assert with_columns is None
-    assert pyarrow_predicate is None
-    assert n_rows is None
-
-    try:
-        res = ir.evaluate(cache={})  # .to_polars()
-    except Exception as e:
-        traceback.print_tb(e.__traceback__)
-        raise
-
-    return res
-
-
-def _execute_with_legate(nt: NodeTraverser, *, config=None) -> Any:
+def _execute_with_legate(nt: NodeTraverser) -> Any:
     """
     A post optimization callback that attempts to execute the plan with legate.
 
@@ -101,13 +78,8 @@ def _execute_with_legate(nt: NodeTraverser, *, config=None) -> Any:
     nt
         NodeTraverser
 
-    config
-        GPUEngine configuration object
-
     Raises
     ------
-    ValueError
-        If the config contains unsupported keys.
     NotImplementedError
         If translation of the plan is unsupported.
 
@@ -115,43 +87,32 @@ def _execute_with_legate(nt: NodeTraverser, *, config=None) -> Any:
     -----
     The NodeTraverser is mutated if the libcudf executor can handle the plan.
     """
+    # NOTE/TODO: This is simplified from cudf-polars, and we should actually
+    # look into making it more similar again (or even re-using it)?
+    # Unfortunately, that is impossible if we don't want the pylibcudf dependency.
     # device = config.device
     # memory_resource = config.memory_resource
-
     # validate_config_options(config.config)
 
-    # with nvtx.annotate(message="ConvertIR", domain="legate_df_polars"):
-    with nullcontext():
-        translator = Translator(nt, pl.GPUEngine(raise_on_fail=True))
-        ir = translator.translate_ir()
-        ir_translation_errors = translator.errors
-        if len(ir_translation_errors):
-            # TODO: Display these errors in user-friendly way.
-            # tracked in https://github.com/rapidsai/cudf/issues/17051
-            unique_errors = sorted(set(ir_translation_errors), key=str)
+    translator = Translator(nt, pl.GPUEngine(raise_on_fail=True))
+    ir = translator.translate_ir()
+    ir_translation_errors = translator.errors
+    if len(ir_translation_errors):
+        # TODO: Display these errors in user-friendly way.
+        # tracked in https://github.com/rapidsai/cudf/issues/17051
+        unique_errors = sorted(set(ir_translation_errors), key=str)
 
-            def format_tb(err):
-                return "\n    ".join(traceback.format_tb(err.__traceback__))
+        def format_tb(err):
+            return "\n    ".join(traceback.format_tb(err.__traceback__))
 
-            formatted_errors = "\n\n".join(
-                f"- {e.__class__.__name__}: {e}: {format_tb(e)!s}"
-                for e in unique_errors
-            )
-            error_message = (
-                "Query execution with legate not possible: unsupported operations."
-                f"\nThe errors were:\n{formatted_errors}"
-            )
-            exception = NotImplementedError(error_message)
-            raise exception
-        # else:
-        #     nt.set_udf(
-        #         partial(
-        #             _callback,
-        #             ir,
-        #             memory_resource=memory_resource,
-        #             config_options=translator.config_options,
-        #             timer=timer,
-        #         )
-        #     )
+        formatted_errors = "\n\n".join(
+            f"- {e.__class__.__name__}: {e}: {format_tb(e)!s}" for e in unique_errors
+        )
+        error_message = (
+            "Query execution with legate not possible: unsupported operations."
+            f"\nThe errors were:\n{formatted_errors}"
+        )
+        exception = NotImplementedError(error_message)
+        raise exception
 
     return ir
