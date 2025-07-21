9a40abb7c0e42a5256ca47b9845da91dfdc22e03
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-07-21 01:46:34 -0700

    Docs build
    Signed-off-by: Rory Mitchell <r.a.mitchell.nz@gmail.com>

--- python/legate_dataframe/lib/sort.pyi
+++ python/legate_dataframe/lib/sort.pyi
@@ -1,11 +1,9 @@
 # Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from pylibcudf.types import NullOrder, Order
-
 from legate_dataframe.lib.core.table import LogicalTable
 
-__all__ = ["NullOrder", "Order", "sort"]
+__all__ = ["sort"]
 
 def sort(
     tbl: LogicalTable,
