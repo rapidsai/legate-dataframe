178a7804152de6ec24ebb5010c6e11053767a0f9
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2025-09-17 02:09:22 -0700

    Remove cupynumeric as test dependency
    Signed-off-by: Rory Mitchell <r.a.mitchell.nz@gmail.com>

--- ci/test_cpu.sh
+++ ci/test_cpu.sh
@@ -9,8 +9,6 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../;
 # Common setup steps shared by Python test jobs (as of writing, just one)
 source ./ci/test_cpu_common.sh
 
-nvidia-smi
-
 rapids-logger "Running C++ tests"
 ./ci/run_ctests_cpu.sh
 
